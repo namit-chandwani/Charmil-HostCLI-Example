cb7d8015afa489441c94744c16bf79658a235bc2
Namit Chandwani <namitchandwani99@gmail.com>
2021-06-03 01:31:11 +0530

    feat: add working code

--- cmd/host/main.go
+++ cmd/host/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/namit-chandwani/Charmil-SDK-POC/pkg/core"
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+const hostKey = "host"
 
 func main() {
-	fmt.Println("Welcome to Host CLI!")
+	cmd := &cobra.Command{
+		Use:          "host",
+		Short:        "Host commands",
+		SilenceUsage: true,
+	}
+
+	hostCLI := core.GetCLI(hostKey)
+	err := hostCLI.AddCommands(cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = core.LoadCommands(cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = doc.GenMarkdownTree(cmd, "./docs/commands")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/namit-chandwani/Charmil-HostCLI-Example
 
 go 1.16
+
+require (
+	github.com/namit-chandwani/Charmil-SDK-POC v0.0.0-20210602194537-b1042eeb80f4
+	github.com/spf13/cobra v1.1.3
+)
